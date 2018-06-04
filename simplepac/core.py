0b5938c94f60fbe560fd920b312e8c3c2ef277f2
Taosky <t@firefoxcn.net>
2018-06-05 00:16:11 +0800

    add userrule &support cidr ip

--- simplepac/core.py
+++ simplepac/core.py
@@ -21,8 +21,20 @@ def get_url_rule(url):
         return None
 
 
+def get_user_rule(rule_file):
+    content = ''
+    try:
+        with open(rule_file, 'r', encoding='utf-8') as rule:
+            content = rule.read()
+    except BaseException as e:
+        print(e)
+
+    return content
+
+
 def filter_rule(rule_text):
-    result = set()
+    url_match = set()
+    cidr_match = []
     for line in rule_text.split('\n'):
         if line.startswith('@@||') \
                 or line.startswith('[') \
@@ -31,20 +43,29 @@ def filter_rule(rule_text):
                 or line.startswith('search') \
                 or line.strip() == '':
             continue
+        elif line.startswith('IP-CIDR,'):
+            icdr = line[8:].strip('\n')
+            ip, end = icdr.split('/')
+            mask = utils.get_mask(end)
+            cidr_match.append([ip, mask])
         else:
-            result.add(line.strip('|.@/').strip('|'))
+            url_match.add(line.strip('|.@/').strip('|'))
 
-    return json.dumps(list(result))
+    return list(url_match), cidr_match
 
 
-def generate(proxy, rules, path):
+def generate(proxy, url_json, cidr_json, path):
     update_time = datetime.now().strftime('%Y %m-%d %H:%M')
 
     pac_content = '''/*
  * Last Update: %s
  * https://github.com/Taosky/simplepac
  */
-var list = %s;
+var Proxy = '%s; DIRECT;';
+
+var urlList = %s;
+
+var cidrList = %s;
 
 function isMatchProxy(url, pattern) {
     try {
@@ -54,15 +75,20 @@ function isMatchProxy(url, pattern) {
     }
 }
 function FindProxyForURL(url, host) {
-    var Proxy = '%s; DIRECT;';
-    for(var i=0, l=list.length; i<l; i++) {
-        if (isMatchProxy(url, list[i])) {
+    for(var i=0, l=cidrList.length; i<l; i++) {
+        if (isInNet(host, cidrList[i][0], cidrList[i][1])){
+            return Proxy;
+        }
+    }
+
+    for(var j=0, m=urlList.length; j<m; j++) {
+        if (isMatchProxy(url, urlList[j])) {
             return Proxy;
         }
     }
     return 'DIRECT';
 }
-    ''' % (update_time,rules, proxy)
+    ''' % (update_time, proxy, url_json, cidr_json)
     try:
         with open(path, 'w', encoding='utf-8') as pac:
             pac.write(pac_content)
@@ -70,15 +96,18 @@ function FindProxyForURL(url, host) {
         print(e)
 
 
-def main(rule_url, proxy, pac_path, custom_rule):
-    rule_data = get_url_rule(rule_url)
-    if rule_data:
-        rule_json = filter_rule(rule_data)
-        generate(proxy, rule_json, pac_path)
+def main(rule_url, proxy, pac_path, user_rule_path):
+    url_rule_data = get_url_rule(rule_url)
+    if url_rule_data:
+        user_rule_data = get_user_rule(user_rule_path)
+        rule_data = '\n'.join([url_rule_data, user_rule_data])
+        url_lst, cidr_result_lst = filter_rule(rule_data)
+        url_json = json.dumps(url_lst)
+        cidr_json = json.dumps(cidr_result_lst)
+        generate(proxy, url_json, cidr_json, pac_path)
 
 
 def run():
-    custom_rule_opt = ''
     parser = argparse.ArgumentParser(description='Generate a simple pac')
     parser.add_argument('-p', '--proxy', required=True, dest='proxy',
                         help='pac proxy like "PROXY 127.0.0.1:8888","SOCKS 127.0.0.1:1080"')
@@ -86,14 +115,18 @@ def run():
                         help='output pac file')
     parser.add_argument('--proxy-rule', dest='proxy_rule',
                         help='proxy rule, Base64 or text, default use gfwlist')
+    parser.add_argument('--user-rule', dest='user_rule',
+                        help='user rule like proxy rule, support cidr like "IP-CIDR,91.108.4.0/22"')
+
     parser.add_argument('--ad-rule', dest='ad_rule', help='ad rule to block ads')
     args = parser.parse_args()
     proxy_opt = args.proxy
     output_opt = args.output
     proxy_rule_opt = args.proxy_rule
+    user_rule_opt = args.user_rule
     ad_rule_opt = args.ad_rule
 
     if not proxy_rule_opt:
         proxy_rule_opt = DEFAULT_PROXY_RULE
 
-    main(proxy_rule_opt, proxy_opt, output_opt, custom_rule_opt)
+    main(proxy_rule_opt, proxy_opt, output_opt, user_rule_opt)
--- simplepac/utils.py
+++ simplepac/utils.py
@@ -9,4 +9,20 @@ def is_base64(text):
     return True
 
 
-__all__ = ['is_base64']
+def get_mask(end):
+    mask = ''
+
+    end = int(end)
+    mask += '.'.join(['255'] * int((end / 8)))
+
+    if (32 - end) % 8 != 0:
+        a = '1' * int(8 - (32 - end) % 8) + '0' * int((32 - end) % 8)
+        mask += '.' + str(int(a, 2))
+
+    b = int((32 - end) / 8) * '.0'
+    mask += b
+
+    return mask
+
+
+__all__ = ['is_base64', 'get_mask']
